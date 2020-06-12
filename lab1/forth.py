b970922c04b62784fd62e0f2663bfa725c9a16f8
Altynay Zhaxylykova <66684636+zhaxylykova@users.noreply.github.com>
2020-06-12 21:47:07 +0600

    Add files via upload

---
+++ lab1/forth.py
@@ -0,0 +1,4 @@
+import math
+number = int(input())
+ans = math.sqrt(number)
+print(ans)
---
+++ lab1/second.py
@@ -0,0 +1,5 @@
+age = int(input())
+rnumber = int(input())
+summa = int(rnumber/100) + rnumber%10
+ans = age + summa
+print(ans)
